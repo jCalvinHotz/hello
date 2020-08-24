710bf0b6a94e16dafd099b0f7285f5efa70c26aa
JCalvinHotz <jhotz@unl.edu>
2020-08-24 01:44:48 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/*
+ * Author: John C Hotz
+ * Date: 8/17/2020
+ *
+ * A simple Hello world program
+ */
+ 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    
+    printf("Hello world\n");
+    
+    return 0;
+}
